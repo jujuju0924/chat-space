5c59e4e6bde7d4fec4b0081c591409be4e43f59b
jujuju0924 <jumpei0924@gmail.com>
2019-12-16 15:08:14 +0900

    更新しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,10 @@
-$(function(){
+$(function () {
+
   function buildHTML(message){
     
-      image = ( message.image.url ) ? `<img class= "lower-message__image" src=${message.image.url} >` : "";
+      image = ( message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
     
-    var html = `<div class=message>
+    var html = `<div class="message" data-message-id="${message.id}"> 
                     <div class="upper-message">
                       <div class="upper-message__user-name">
                       ${message.user_name}
@@ -19,8 +20,6 @@ $(function(){
                       ${image}
                     </div>
                   </div> `
-
-                  
     return html
   }
   $('.new_message').on('submit', function(e){
@@ -29,8 +28,8 @@ $(function(){
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      url: url,  
+      type: 'POST',  
       data: formData,
       dataType: 'json',
       processData: false,
@@ -49,4 +48,29 @@ $(function(){
       alert('error');
     });
   });
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
+
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
+      })
+      .done(function (messages) { 
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
+
