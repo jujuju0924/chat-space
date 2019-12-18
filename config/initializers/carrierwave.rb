901b9eb183ade8418c2e8b28480618caf6d4b15d
jujuju0924 <jumpei0924@gmail.com>
2019-12-18 14:46:18 +0900

    comit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'jumpei0924'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jumpei0924'
 end
